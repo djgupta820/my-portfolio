6a03e9f537c43cccf8d0df64d3f7c65af7f00b09
Dheeraj Gupta <djgupta820@gmail.com>
2023-05-15 19:57:22 +0530

    project complete

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 let mainMenu = document.querySelector('#mainMenu')
 let openMenu = document.querySelector('#openMenu')
 let closeMenu = document.querySelector('#closeMenu')
+let html = document.querySelector('#html')
+let css = document.querySelector('#css')
+let js = document.querySelector('#js')
+let php = document.querySelector('#php')
+let sql = document.querySelector('#sql')
+let java = document.querySelector('#java')
+let cpp = document.querySelector('#cpp')
+let git = document.querySelector('#git')
 
 openMenu.addEventListener('click', ()=>{
     mainMenu.style.display = 'flex';
@@ -10,3 +18,50 @@ openMenu.addEventListener('click', ()=>{
 closeMenu.addEventListener('click', ()=>{
     mainMenu.style.right = '-80%';
 })
+
+html.addEventListener('mouseover', (e)=>{
+    moveProgress(e.target.value, e.target)
+})
+
+css.addEventListener('mouseover', (e)=>{
+    moveProgress(e.target.value, e.target)
+})
+
+js.addEventListener('mouseover', (e)=>{
+    moveProgress(e.target.value, e.target)
+})
+
+sql.addEventListener('mouseover', (e)=>{
+    moveProgress(e.target.value, e.target)
+})
+
+php.addEventListener('mouseover', (e)=>{
+    moveProgress(e.target.value, e.target)
+})
+
+java.addEventListener('mouseover', (e)=>{
+    moveProgress(e.target.value, e.target)
+})
+
+git.addEventListener('mouseover', (e)=>{
+    moveProgress(e.target.value, e.target)
+})
+
+cpp.addEventListener('mouseover', (e)=>{
+    moveProgress(e.target.value, e.target)
+})
+
+function moveProgress(val, element){
+    console.log(element)
+    var id = setInterval(move, 100)
+    let i = 0
+    function move(){
+        if(i >= val){
+            clearInterval(id)
+            i=0
+        }else{
+            i++
+            element.style.value = i
+        }
+    }
+}
